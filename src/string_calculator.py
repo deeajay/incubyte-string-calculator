c16aca88fbc6f49036794e48001064b083cfd72f
Ajay Deekonda <ajay@gharista.in>
2025-08-22 13:18:43 +0530

    added custom delimited and negative number test cases.

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -4,13 +4,27 @@ class StringCalculator:
             return 0
         if numbers is None:
             return 0
+        # Handle custom delimiters
         delimiter = ","
+        if numbers.startswith("//"):
+            delimiter_end = numbers.index("\n")
+            delimiter = numbers[2:delimiter_end]
+            numbers = numbers[delimiter_end + 1:]
+        
         numbers = numbers.replace("\n", delimiter)
-        parts = numbers.split(",")
+        
+        parts = numbers.split(delimiter)
         total = 0
+        negatives = []
+
         for p in parts:
-            p = p.strip()
-            if p == "":
-                continue
-            total += int(p)
+            p= 0 if p==''else  int(p)
+            if p < 0:
+                negatives.append(p)
+            else:
+                total += p
+
+        if negatives:
+            raise ValueError("Negative numbers not allowed: " + ", ".join(map(str, negatives)))
+        
         return total
--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -25,3 +25,43 @@ def test_multiple_amount_of_numbers(calc):
 
 def test_newlines_are_delimiters(calc):
     assert calc.add("1\n2,3") == 6
+
+def test_newlines_with_large_numbers(calc):
+    assert calc.add("300\n501,1000") == 1801
+
+def test_custom_single_char_delimiter(calc):
+    assert calc.add("//;\n1;2") == 3
+
+def test_none_returns_0(calc):
+    assert calc.add(None) == 0
+
+def test_spaces_are_ignored(calc):
+    assert calc.add("1, 2,3") == 6
+
+def test_leading_trailing_spaces(calc):
+    assert calc.add(" 4 ,5 ") == 9
+
+def test_multiple_char_custom_delimiter(calc):
+    assert calc.add("//***\n1***2***3") == 6
+
+def test_consecutive_delimiters_ignore_empty(calc):
+    assert calc.add("1,,2") == 3
+
+def test_custom_delimiter_dot(calc):
+    assert calc.add("//.\n2.3.4") == 9
+
+def test_custom_single_char_delimiter(calc):
+    assert calc.add("//;\n1;2") == 3
+
+def test_negative_numbers_raise_with_list(calc):
+    with pytest.raises(Exception) as exc:
+        calc.add("1,-2,3,-4")
+    assert "Negative numbers not allowed: -2, -4" in str(exc.value)
+
+def test_when_single_negative_number_present(calc):
+    with pytest.raises(Exception) as exc:
+        calc.add("1,2,3,-4")
+    assert "Negative numbers not allowed: -4" in str(exc.value)
+
+def test_when_zero_with_negative_number_present(calc):
+    assert calc.add("1,2,3,-0") == 6
